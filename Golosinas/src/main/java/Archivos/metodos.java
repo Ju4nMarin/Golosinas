d4448f8468a1366c377cdf388147ed205d6cd306
Ju4nMarin <80414695+Ju4nMarin@users.noreply.github.com>
2021-03-14 10:44:05 -0500

    Add files via upload

---
+++ Golosinas/src/main/java/Archivos/metodos.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Archivos;
+
+/**
+ *
+ * @author JUAN MARIN
+ */
+public class metodos {
+
+    String[][] nombresGolosinas = {
+        {"KitKat", "Chicles de fresa", "Lacasitos", "Palotes"},
+        {"Kinder Bueno", "Bolsa variada Haribo", "Chetoos", "Twix"},
+        {"Kinder Bueno", "M&M'S", "Papa Delta", "Chicles de menta"},
+        {"Lacasitos", "Crunch", "Milkybar", "KitKat"}
+
+    };
+
+    double[][] precio = {
+        {1000, 200, 500, 950},
+        {1800, 100, 120, 100},
+        {1750, 130, 120, 800},
+        {1500, 110, 720, 350}
+
+    };
+
+    double[][] cantidad = {
+        {5, 5, 5, 5},
+        {5, 5, 5, 5},
+        {5, 5, 5, 5},
+        {5, 5, 5, 5}
+
+    };
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
